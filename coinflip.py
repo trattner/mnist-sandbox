f5f1691fd9780d61366d36646ccf2e5e04d1a6cb
atratt <yo@atratt.com>
2019-01-24 11:55:50 -0600

    no match

--- coinflip.py
+++ coinflip.py
@@ -18,12 +18,13 @@ trials = 1000
 samples = 30000
 p_list = []
 no_match_list = []
+p_ht_list = []
 
 for j in range(samples):
     outcomes = []
     no_match_counter = 0
     head_count = 0
-    total_count = 0
+    total_head_count = 0
     for i in range(trials):
         students = []
         for i in range(n):
@@ -39,19 +40,28 @@ for j in range(samples):
             outcomes.append(students[k])
             if students[k] == 'h':
                 head_count += 1
-            total_count += 1
+            total_head_count += 1
 
             #print 'student ' + str(k)+ ' had ' + str(students[k])
         else:
             #print 'no match'
             no_match_counter += 1
-    p = head_count / float(total_count)
-    p_nm = no_match_counter / float(total_count)
+    p = head_count / float(total_head_count) #p of problem
+    p_ht = head_count / float(trials)#p of heads overall including no match situations
+    p_nm = no_match_counter / float(trials)
     p_list.append(p)
     no_match_list.append(p_nm)
+    p_ht_list.append(p_ht)
 
+print '\nProblem as worded:'
 print np.average(p_list)
 print np.std(p_list)
 
+print '\nNo match:'
 print np.average(no_match_list)
 print np.std(no_match_list)
+
+print '\nOverall probability of scenario:'
+print np.average(p_ht_list)
+print np.std(p_ht_list)
+print '\n'
